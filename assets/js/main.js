74b6634527f971a7e6e278fdb08a32fb75202876
Gabriel Henrique da Silva <gabriel04gh1.gh@gmail.com>
2021-04-14 15:44:35 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,62 @@
+function myScopo() {
+    const form = document.querySelector('.form')
+    const alturaHtml = document.querySelector('#altura')
+    const pesoHtml = document.querySelector('#peso')
+    const resultHtml = document.querySelector('.result')
+
+    form.addEventListener('submit', submitForm)
+
+    function submitForm(event) {
+        event.preventDefault()
+        prevent(pesoHtml.value, alturaHtml.value)
+    }
+
+    function prevent(peso, altura) {
+        if (isNaN(peso)) {
+            resultHtml.innerHTML = `<p class="danger">${peso} não é um valor válido</p>`
+            pesoHtml.focus()
+        } else if (isNaN(altura)) {
+            alturaHtml.focus()
+            resultHtml.innerHTML = `<p class="danger">${altura} não é um valor válido</p>`
+        } else if (altura == ``){
+            resultHtml.innerHTML = '<p class="danger">Insira um valor na altura.</p>'
+            alturaHtml.focus()
+        } else if (peso == ``) {
+            resultHtml.innerHTML = '<p class="danger">Insira um valor no peso.</p>'
+            pesoHtml.focus()
+        } else {
+            imcResult(peso, altura)
+        }
+
+
+    }
+
+    function imcResult(p, a) {
+        a = (parseFloat(a)) ** 2
+        p = parseFloat(p)
+        const result = p / a
+
+        let i;
+        if (result <= 18.5) {
+            i = 'Abaixo do peso'
+        } else if (result < 25) {
+            i = 'Peso normal'
+        } else if (result < 30) {
+            i = 'Sobrepeso'
+        } else if (result < 35) {
+            i = 'Obesidade grau 1'
+        } else if (result < 40) {
+            i = 'Obesidade grau 2'
+        } else {
+            i = 'Obesidade grau 3'
+        }
+
+        resultHtml.innerHTML = `<p class="true">${i} (IMC: ${result.toFixed(2)})</p>`
+
+        pesoHtml.value = ``
+        alturaHtml.value = ``
+    }
+
+}
+
+myScopo()
